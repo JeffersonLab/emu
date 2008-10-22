67a0499d26e49caf6ac657258450e1d7c1dba6ef
Graham Heyes <heyes@jlab.org>
2008-10-22 12:36:44 +0000

    Comb out the warnings from this code and general tidy.

--- javasrc/org/jlab/coda/support/transport/DataChannelImplCMsg.java
+++ javasrc/org/jlab/coda/support/transport/DataChannelImplCMsg.java
@@ -16,9 +16,9 @@ import org.jlab.coda.support.control.CmdExecException;
 import org.jlab.coda.support.evio.DataRecord;
 import org.jlab.coda.support.log.Logger;
 
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -44,68 +44,79 @@ public class DataChannelImplCMsg implements DataChannel {
     /** Field dataThread */
     private Thread dataThread;
 
-    /** Field size */
-    private final int size = 20000;
+    /** Field size - the default size of the buffers used to receive data */
+    private int size = 20000;
 
-    /** Field full */
+    /** Field capacity - number of records that will fit in the fifos */
+    private int capacity = 40;
+
+    /** Field full - filled buffer queue */
     private final BlockingQueue<DataRecord> full;
 
-    /** Field empty */
+    /** Field empty - empty buffer queue */
     private final BlockingQueue<DataRecord> empty;
+
+    /** Field out */
+    private DataOutputStream out;
+
+    /** Field in */
+    private DataInputStream in;
 
     /**
      * Constructor DataChannelImplSO creates a new DataChannelImplSO instance.
      *
-     * @param pname     of type String
+     * @param name      of type String
      * @param transport of type DataTransport
+     * @throws TransportException - unable to create buffers or socket.
      */
-    DataChannelImplCMsg(String pname, TransportImplCMsg transport) {
-        System.out.println("DataChannelImplCMsg : " + pname);
+    DataChannelImplCMsg(String name, TransportImplCMsg transport) throws TransportException {
+
         this.transport = transport;
-        name = pname;
+        this.name = name;
 
-        String capacityS = "40";
-        String tmp = transport.getAttr("pool");
-        if (capacityS != null) capacityS = tmp;
+        try {
+            capacity = transport.getIntAttr("capacity");
+        } catch (Exception e) {
+            Logger.info(e.getMessage() + " default to " + capacity + " records.");
+        }
 
-        int capacity = Integer.valueOf(capacityS);
+        try {
+            size = transport.getIntAttr("size");
+        } catch (Exception e) {
+            Logger.info(e.getMessage() + " default to " + size + " byte records.");
+        }
 
         full = new ArrayBlockingQueue<DataRecord>(capacity);
         empty = new ArrayBlockingQueue<DataRecord>(capacity);
 
-        for (int ix = 0; ix < capacity; ix++) {
-            DataRecord d = new DataRecord(size);
-
+        // Fill the queue with records of default size
+        while (empty.remainingCapacity() > 0) {
             try {
-                empty.put(d);
+                empty.put(new DataRecord(size));
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                throw new TransportException("DataChannelImplCMsg :can't fill the pool of empty buffers ", e);
             }
         }
 
-        if (transport.isServer()) {
+        // If we are a server the accept helper in the transport implementation will
+        // handle connections
+        if (!transport.isServer()) {
 
-        } else {
             try {
-                System.out.println("Create data socket : " + transport.getHost() + " " + transport.getPort() + " " + transport);
-
                 dataSocket = new Socket(transport.getHost(), transport.getPort());
 
                 dataSocket.setTcpNoDelay(true);
-                OutputStream out = dataSocket.getOutputStream();
-                DataOutputStream dout = new DataOutputStream(out);
 
-                System.out.println("Send name over data socket : " + name);
-                dout.write(name.length());
-
-                dout.write(name.getBytes());
+                out = new DataOutputStream(dataSocket.getOutputStream());
+                in = new DataInputStream(dataSocket.getInputStream());
+                // Always write a 4 byte length followed by data
+                out.write(name.length());
+                out.write(name.getBytes());
 
                 startOutputHelper();
 
-            } catch (SocketException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            } catch (Exception e) {
+                throw new TransportException("DataChannelImplCMsg : Cannot create data channel", e);
             }
         }
 
@@ -152,22 +163,33 @@ public class DataChannelImplCMsg implements DataChannel {
      * Method setDataSocket sets the dataSocket of this DataChannelImplSO object.
      *
      * @param incoming the dataSocket of this DataChannelImplSO object.
+     * @throws TransportException - the socket closed while we were in here, unlikely.
      */
-    public void setDataSocket(Socket incoming) {
+    public void setDataSocket(Socket incoming) throws TransportException {
         this.dataSocket = incoming;
+        try {
+            out = new DataOutputStream(dataSocket.getOutputStream());
+            in = new DataInputStream(dataSocket.getInputStream());
+        } catch (Exception e) {
+            throw new TransportException("setDataSocket failed ", e);
+        }
     }
 
+    /**
+     * <pre>
+     * Class <b>DataInputHelper </b>
+     * This class reads data from the socket and queues it on the fifo.
+     * It checks that the data buffer is large enough and allocates a
+     * bigger buffer if needed.
+     * <p/>
+     * TODO : the acknowledge written is fixed at 0xaa, it should be some feedback to the sender
+     * </pre>
+     */
     private class DataInputHelper implements Runnable {
 
         /** Method run ... */
-        @SuppressWarnings({"InfiniteLoopStatement"})
         public void run() {
             try {
-
-                DataInputStream in = new DataInputStream(dataSocket
-                        .getInputStream());
-
-                OutputStream os = dataSocket.getOutputStream();
 
                 while (dataSocket.isConnected()) {
                     // take an empty buffer
@@ -177,7 +199,7 @@ public class DataChannelImplCMsg implements DataChannel {
 
                     int rl = dr.getRecordLength();
                     int bl = dr.getBufferLength();
-                    System.out.println("Recieved length : " + rl + " buffer is " + bl);
+
                     // rl is the count of long words in the buffer.
                     // the buffer needs to be (rl+1)*4 bytes long
                     // the extra 4 bytes hold the length
@@ -191,46 +213,47 @@ public class DataChannelImplCMsg implements DataChannel {
                     // read in the payload, remember to offset 4 bytes so
                     // we don't overwrite the length
                     in.readFully(dr.getData(), 4, rl * 4);
-                    System.out.println("got all data, send ack");
-                    os.write(0xaa);
+
+                    out.write(0xaa);
                     full.put(dr);
                 }
+                Logger.warn(name + " - data socket disconnected");
             } catch (Exception e) {
                 Logger.warn("DataInputHelper exit " + e.getMessage());
+                e.printStackTrace();
             }
 
         }
 
     }
 
+    /**
+     * <pre>
+     * Class <b>DataOutputHelper </b>
+     * </pre>
+     * Handles sending data.
+     * TODO : the ack should be some feedback from the receiver.
+     */
     private class DataOutputHelper implements Runnable {
 
         /** Method run ... */
-        @SuppressWarnings({"InfiniteLoopStatement"})
         public void run() {
             try {
-
-                DataOutputStream out = new DataOutputStream(dataSocket
-                        .getOutputStream());
-
                 DataRecord d;
-                int len;
-                int ack;
-                InputStream is = dataSocket.getInputStream();
 
-                while (true) {
+                while (dataSocket.isConnected()) {
                     d = full.take();
-                    len = d.getRecordLength();
-                    System.out.println("DataSocket " + dataSocket + " open = " + dataSocket.isConnected() + " length " + d.getData());
+
                     out.write(d.getData(), 0, 4);
-                    out.write(d.getData(), 4, len * 4);
-                    ack = is.read();
+                    out.write(d.getData(), 4, d.getRecordLength() * 4);
+                    int ack = in.read();
                     if (ack != 0xaa) {
                         throw new CmdExecException("DataOutputHelper : ack = " + ack);
                     }
                     d.setRecordLength(0);
                     empty.put(d);
                 }
+                Logger.warn(name + " - data socket disconnected");
             } catch (Exception e) {
                 Logger.warn("DataOutputHelper exit " + e.getMessage());
             }
