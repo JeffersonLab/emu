ab93e9fedcc08b12e12bb1b057afd243016df5aa
Graham Heyes <heyes@jlab.org>
2009-06-10 13:34:40 +0000

    Remove referendes to home directory

--- etc/start_SingleEmu.sh
+++ etc/start_SingleEmu.sh
@@ -1,2 +1,1 @@
-#/usr/bin/java -DDebugUI -Dname=SingleEmu -Ddebug=info -jar /daqfs/home/heyes/develop/install/lib/emu.jar
-/usr/bin/java -DDebugUI -Dname=SingleEmu -jar /daqfs/home/heyes/develop/install/lib/emu.jar
+/usr/bin/java -DDebugUI -Dname=SingleEmu -jar $INSTALL_DIR/lib/emu.jar
