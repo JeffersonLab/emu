e0d7eb97fe59ec0748e6a25a9d632edef7ece0f5
Carl Timmer <timmer@jlab.org>
2009-08-13 15:14:19 +0000

    move classloader from EmuModuleFactory to its own public class

--- javasrc/org/jlab/coda/emu/EmuModuleFactory.java
+++ javasrc/org/jlab/coda/emu/EmuModuleFactory.java
@@ -53,12 +53,12 @@ public class EmuModuleFactory implements StatedObject {
     private static final Collection<org.jlab.coda.emu.EmuModule> modules = new Vector<org.jlab.coda.emu.EmuModule>();
 
     /** Field classloader */
-    private ModuleClassLoader classloader;
+    private EmuClassLoader classloader;
 
     /** Field state */
     private State state = CODAState.UNCONFIGURED;
 
-    /** Field TRANSPORT_FACTORY */
+    /** Field TRANSPORT_FACTORY - singleton */
     private final static DataTransportFactory TRANSPORT_FACTORY = new DataTransportFactory();
 
     /**
@@ -132,7 +132,7 @@ public class EmuModuleFactory implements StatedObject {
 
                 // Load "untrusted" java code from URLs - each of which
                 // represents a directory or JAR file to search.
-                classloader = new ModuleClassLoader(locations);
+                classloader = new EmuClassLoader(locations);
 
                 // remove all existing modules from collection
                 modules.clear();
@@ -156,7 +156,7 @@ public class EmuModuleFactory implements StatedObject {
                 // Thus any new classloader would ask its parent to load the file, but the
                 // parent (system classloader) would never be able to see the modules.jar
                 // in the classpath (set by -cp option or in CLASSPATH env variable)
-                // so it would always delegate the loading to the new classloader.
+                // so it would always delegate the loading back to the new classloader.
                 // And things worked fine. However, if the emu was run like:
                 //       java -cp ...... org.jlab.coda.emu.Emu
                 // then the system class loader would find the modules in the classpath and
@@ -398,105 +398,4 @@ public class EmuModuleFactory implements StatedObject {
 
 }
 
-/**
- * This class is a custom classLoader which delegates all loading to its parent (in this case, system)
- * classLoader except for a single named class which is "refound" and reloaded using standard URLClassLoader
- * means. The reloading of that single class in ensured if and only if a new object of this type is
- * used each time a class needs reloading.<p>
- *
- * <b>Dynamic Class Reloading</b><p>
- * (http://tutorials.jenkov.com/java-reflection/dynamic-class-loading-reloading.html)<p>
- *
- * Dynamic class reloading is a bit more challenging. Java's builtin Class loaders always check if a class is already
- * loaded before loading it. Reloading the class is therefore not possible using Java's builtin class loaders.
- * To reload a class you will have to implement your own ClassLoader subclass.<p>
- *
- * Even with a custom subclass of ClassLoader you have a challenge. Every loaded class needs to be linked.
- * This is done using the ClassLoader.resolve() method. This method is final, and thus cannot be overridden in your
- * ClassLoader subclass. The resolve() method will not allow any given ClassLoader instance to link the same class
- * twice. Therefore, everytime you want to reload a class you must use a new instance of your ClassLoader subclass.
- * This is not impossible, but necessary to know when designing for class reloading.<p>
- *
- * The trick is to:
- * <ul>
- * <li>  Use an interface as the variable type, and just reload the implementing class, or
- * <li>  Use a superclass as the variable type, and just reload a subclass.
- * </ul><p>
- * Either of these two methods will work if the type of the variable, the interface or superclass,
- * is not reloaded when the implementing class or subclass is reloaded.<p>
- *
- * To make this work you will of course need to implement your class loader to let the interface or superclass be
- * loaded by its parent. When your class loader is asked to load the MyObject class, it will also be asked to load
- * the MyObjectInterface class, or the MyObjectSuperclass class, since these are referenced from within the MyObject
- * class. Your class loader must delegate the loading of those classes to the same class loader that loaded the class
- * containing the interface or superclass typed variables.<p>
- *
- */
-class ModuleClassLoader extends URLClassLoader {
-
-    /** Name of the single class we want loaded. */
-    String classToLoad;
-
-    /**
-     * Constructor. The system class loader is assumed to be the parent.
-     * @param urls URLs to search for the classes of interest.
-     */
-    public ModuleClassLoader(URL[] urls) {
-        super(urls);
-    }
-
-    /**
-     * Constructor.
-     * @param urls URLs to search for the classes of interest.
-     * @param parent parent classloader
-     */
-    public ModuleClassLoader(URL[] urls, ClassLoader parent) {
-        super(urls, parent);
-    }
-    
-    /**
-     * Set the name of the single class we want to load.
-     * @param className name of the single class we want to load
-     */
-    public void setClassToLoad(String className) {
-        classToLoad = className;
-    }
-
-    /**
-     * Method to load the class of interest. This is called many
-     * times by the system - once for each class that the class
-     * we are trying to load references.
-     *
-     * @param name name of class to load
-     * @return Class object
-     * @throws ClassNotFoundException if code for class cannot be found
-     */
-    public Class loadClass(String name) throws ClassNotFoundException {
-
-        // Default to standard URLClassLoader behavior
-        if (classToLoad == null) super.loadClass(name);
-
-        // We only want to load 1 specific class. Everything else needs to
-        // be loaded by the system classloader (the parent in this case).
-        // We especially do not want to load "org.jlab.coda.emu.EmuModule"
-        // since that will not be the same class as the identically named
-        // one loaded by the system loader. Because all modules implement
-        // the EmuModule interface, this loader will be asked to load it.
-        // Simply pass the request back to the parent and we're OK. Now
-        // there's only one version of EmuModule that everyone agrees on.
-        if(!classToLoad.equals(name)) {
-//System.out.println("ModuleClassLoader: have parent load " + name);
-            return getParent().loadClass(name);
-        }
-
-        // Cannot call super.loadClass(name) since that calls ClassLoader.loadClass(name)
-        // (since URLClassLoader never overwrites it), which in turn delegates the finding
-        // to the parent. We want to use the URLClassLoader to find it from scratch so call
-        // the findClass() method directly.
-        
-//System.out.println("ModuleClassLoader: I will load/find " + name);
-        return findClass(name);
-    }
-
-}
 
