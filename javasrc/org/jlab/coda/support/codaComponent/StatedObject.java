1af128a347b9b919292d83e5b9e033062549c7a4
Carl Timmer <timmer@jlab.org>
2009-08-14 15:07:32 +0000

    javadoc

--- javasrc/org/jlab/coda/support/codaComponent/StatedObject.java
+++ javasrc/org/jlab/coda/support/codaComponent/StatedObject.java
@@ -3,18 +3,18 @@ package org.jlab.coda.support.codaComponent;
 import org.jlab.coda.support.control.State;
 
 /**
- * An CODA component that implements this interface has a name and a state.
+ * An object that implements this interface has a name and a state.
  */
 public interface StatedObject {
     /**
-     * Get the name of this CODA component
-     * @return the name of this CODA component
+     * Get the name of this object.
+     * @return the name of this object
      */
     public String name();
 
     /**
-     * Get the state of this CODA component.
-     * @return the state of this CODA componen
+     * Get the state of this object.
+     * @return the state of this Cobject
      */
     public State state();
 }
