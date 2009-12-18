9001ee60df463b8fa5badd1c195a26d71decfe20
Carl Timmer <timmer@jlab.org>
2009-12-18 17:03:54 +0000

    add test code to repository

---
+++ java/org/jlab/coda/test/ReceiverCmsg.java
@@ -0,0 +1,274 @@
+package org.jlab.coda.test;
+
+import org.jlab.coda.cMsg.*;
+import org.jlab.coda.jevio.EvioBank;
+import org.jlab.coda.jevio.EvioException;
+import org.jlab.coda.jevio.ByteParser;
+
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.stream.XMLOutputFactory;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.nio.ByteOrder;
+
+/**
+ * This class is designed to receive output from an EMU.
+ * User: timmer
+ * Date: Dec 4, 2009
+ */
+public class ReceiverCmsg {
+
+    private String  subject = "BitBucket";
+    private String  type = "data";
+    private String  name = "BitBucket";
+    private String  channelName = "BitBucket_SOCKET"; // channelName is subject
+    private String  description = "place to dump EMU data";
+    private String  UDL;
+
+    private int     delay, count = 5000, timeout = 3000; // 3 second default timeout
+    private boolean debug;
+
+    private cMsg coda;
+    private cMsgSubscriptionHandle sub;
+    ByteParser parser;
+
+
+    /** Constructor. */
+    ReceiverCmsg(String[] args) throws cMsgException {
+        decodeCommandLine(args);
+        parser = new ByteParser();
+    }
+
+
+    /**
+     * Method to decode the command line used to start this application.
+     * @param args command line arguments
+     */
+    private void decodeCommandLine(String[] args) {
+
+        // loop over all args
+        for (int i = 0; i < args.length; i++) {
+
+            if (args[i].equalsIgnoreCase("-h")) {
+                usage();
+                System.exit(-1);
+            }
+            else if (args[i].equalsIgnoreCase("-n")) {
+                name = args[i + 1];
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-u")) {
+                UDL= args[i + 1];
+                i++;
+            }
+            // s - stands for sender
+            else if (args[i].equalsIgnoreCase("-s")) {
+                subject = args[i + 1];
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-t")) {
+                type= args[i + 1];
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-c")) {
+                count = Integer.parseInt(args[i + 1]);
+                if (count < 1)
+                    System.exit(-1);
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-to")) {
+                timeout = Integer.parseInt(args[i + 1]);
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-delay")) {
+                delay = Integer.parseInt(args[i + 1]);
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-debug")) {
+                debug = true;
+            }
+            else {
+                usage();
+                System.exit(-1);
+            }
+        }
+
+        return;
+    }
+
+
+    /** Method to print out correct program command line usage. */
+    private static void usage() {
+        System.out.println("\nUsage:\n\n" +
+            "   java ReceiverCmsg\n" +
+            "        [-n <name>]          set client name\n"+
+            "        [-u <UDL>]           set UDL to connect to cMsg\n" +
+            "        [-s <sender>]        set sender of EMU data\n" +
+            "        [-t <type>]          set type of subscription/sent messages\n" +
+            "        [-c <count>]         set # of messages to get before printing output\n" +
+            "        [-to <time>]         set timeout\n" +
+            "        [-delay <time>]      set time in millisec between sending of each message\n" +
+            "        [-debug]             turn on printout\n" +
+            "        [-h]                 print this help\n");
+    }
+
+
+    /**
+     * Run as a stand-alone application.
+     */
+    public static void main(String[] args) {
+        try {
+            ReceiverCmsg receiver = new ReceiverCmsg(args);
+            receiver.run();
+        }
+        catch (cMsgException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+
+    /**
+     * Method to convert a double to a string with a specified number of decimal places.
+     *
+     * @param d double to convert to a string
+     * @param places number of decimal places
+     * @return string representation of the double
+     */
+    private static String doubleToString(double d, int places) {
+        if (places < 0) places = 0;
+
+        double factor = Math.pow(10,places);
+        String s = "" + (double) (Math.round(d * factor)) / factor;
+
+        if (places == 0) {
+            return s.substring(0, s.length()-2);
+        }
+
+        while (s.length() - s.indexOf(".") < places+1) {
+            s += "0";
+        }
+
+        return s;
+    }
+
+
+
+    /**
+     * Method to wait on string from keyboard.
+     * @param s prompt string to print
+     * @return string typed in keyboard
+     */
+    public String inputStr(String s) {
+        String aLine = "";
+        BufferedReader input =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(s);
+        try {
+            aLine = input.readLine();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return aLine;
+    }
+
+
+    /**
+     * This class defines the callback to be run when a message matching our subscription arrives.
+     */
+    class myCallback extends cMsgCallbackAdapter {
+
+        public void callback(cMsgMessage msg, Object userObject) {
+            byte[] data = msg.getByteArray();
+            if (data == null) {
+                System.out.println("ReceiverCmsg: got bogus message");
+                return;
+            }
+
+            count++;
+
+            try {
+                ByteOrder byteOrder = ByteOrder.BIG_ENDIAN;
+
+                if (msg.getByteArrayEndian() == cMsgConstants.endianLittle) {
+                    System.out.println("NEED TO SWAP DUDE !!!");
+                    byteOrder = ByteOrder.LITTLE_ENDIAN;
+                }
+                else {
+                    System.out.println("ENDIAN IS BIG DUDE !!!");
+                }
+
+                EvioBank bank = parser.parseEvent(data, byteOrder);
+                StringWriter sw = new StringWriter(1000);
+                XMLStreamWriter xmlWriter = XMLOutputFactory.newInstance().createXMLStreamWriter(sw);
+                bank.toXML(xmlWriter);
+                System.out.println("Receiving msg:\n" + sw.toString());
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
+
+
+    /**
+     * This method is executed as a thread.
+     */
+    public void run() throws cMsgException {
+
+        if (debug) {
+            System.out.println("Running ReceiverCmsg as EMU's bitbucket\n");
+        }
+
+        // Get the UDL for connection to the cMsg server. If none given,
+        // cMSGPortal defaults to using UDL = "cMsg:cMsg://localhost/cMsg/test".
+        if (UDL == null) UDL = System.getProperty("cmsgUDL");
+        if (UDL == null) UDL = "cMsg://localhost/cMsg/test";
+
+        // connect to cMsg server
+        coda = new cMsg(UDL, name, description);
+        coda.connect();
+
+        myCallback cb = new myCallback();
+        sub = coda.subscribe(subject, type, cb, null);
+
+        // enable message reception
+        coda.start();
+
+        // inputStr("Enter when time to GO");
+
+        // variables to track incoming message rate
+        double freq, freqAvg;
+        long   totalT=0, totalC=0, period = 2000; // millisec
+
+        while (true) {
+            count = 0;
+
+            // wait
+            try { Thread.sleep(period); }
+            catch (InterruptedException e) {}
+
+            freq    = (double)count/period*1000;
+            totalT += period;
+            totalC += count;
+            freqAvg = (double)totalC/totalT*1000;
+
+            if (debug) {
+                System.out.println("count = " + count + ", total = " + totalC);
+                System.out.println("freq  = " + doubleToString(freq, 1) + " Hz, Avg = " +
+                                   doubleToString(freqAvg, 1) + " Hz");
+            }
+
+            if (!coda.isConnected()) {
+                // if still not connected, quit
+                System.out.println("No longer connected to domain server, quitting");
+                System.exit(-1);
+            }
+        }
+
+    }
+
+
+}
---
+++ java/org/jlab/coda/test/SenderCmsg.java
@@ -0,0 +1,348 @@
+package org.jlab.coda.test;
+
+import org.jlab.coda.cMsg.cMsgException;
+import org.jlab.coda.cMsg.cMsg;
+import org.jlab.coda.cMsg.cMsgMessage;
+import org.jlab.coda.cMsg.cMsgConstants;
+import org.jlab.coda.jevio.*;
+import org.jlab.coda.support.data.Evio;
+
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import java.net.Socket;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * This class is designed to send data to an EMU.
+ * User: timmer
+ * Date: Dec 4, 2009
+ * Time: 10:39:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SenderCmsg {
+
+    private String  subject  = "ROC1";
+    private String  subject1 = "ROC1";
+    private String  subject2 = "ROC2";
+    private String  type = "data";
+    private String  name = "ROC1";
+    private String  channelName = "SingleEmu_SOCKET"; // channelName is subject
+    private String  description = "place to send data to EMU";
+    private String  UDL;
+
+    private int     delay, timeout = 1000; // 1 second default timeout
+    private boolean debug;
+    private boolean stopSending;
+    private cMsg coda;
+
+
+    /** Constructor. */
+    SenderCmsg(String[] args) {
+        decodeCommandLine(args);
+    }
+
+
+    /**
+     * Method to decode the command line used to start this application.
+     * @param args command line arguments
+     */
+    private void decodeCommandLine(String[] args) {
+
+        // loop over all args
+        for (int i = 0; i < args.length; i++) {
+
+            if (args[i].equalsIgnoreCase("-h")) {
+                usage();
+                System.exit(-1);
+            }
+            else if (args[i].equalsIgnoreCase("-n")) {
+                name = args[i + 1];
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-u")) {
+                UDL= args[i + 1];
+                i++;
+            }
+            // s - stands for sender
+            else if (args[i].equalsIgnoreCase("-s")) {
+                subject = args[i + 1];
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-t")) {
+                type= args[i + 1];
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-to")) {
+                timeout = Integer.parseInt(args[i + 1]);
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-delay")) {
+                delay = Integer.parseInt(args[i + 1]);
+                i++;
+            }
+            else if (args[i].equalsIgnoreCase("-debug")) {
+                debug = true;
+            }
+            else {
+                usage();
+                System.exit(-1);
+            }
+        }
+
+        return;
+    }
+
+
+    /** Method to print out correct program command line usage. */
+    private static void usage() {
+        System.out.println("\nUsage:\n\n" +
+            "   java SenderCmsg\n" +
+            "        [-n <name>]          set client name\n"+
+            "        [-u <UDL>]           set UDL to connect to cMsg\n" +
+            "        [-s <subject>]       set subject of data messages\n" +
+            "        [-t <type>]          set type of data messages\n" +
+            "        [-to <time>]         set timeout\n" +
+            "        [-delay <time>]      set time in millisec between sending of each message\n" +
+            "        [-debug]             turn on printout\n" +
+            "        [-h]                 print this help\n");
+    }
+
+
+    /**
+     * Run as a stand-alone application.
+     */
+    public static void main(String[] args) {
+        try {
+            SenderCmsg sender = new SenderCmsg(args);
+            sender.run();
+        }
+        catch (cMsgException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+
+    /**
+     * Method to convert a double to a string with a specified number of decimal places.
+     *
+     * @param d double to convert to a string
+     * @param places number of decimal places
+     * @return string representation of the double
+     */
+    private static String doubleToString(double d, int places) {
+        if (places < 0) places = 0;
+
+        double factor = Math.pow(10,places);
+        String s = "" + (double) (Math.round(d * factor)) / factor;
+
+        if (places == 0) {
+            return s.substring(0, s.length()-2);
+        }
+
+        while (s.length() - s.indexOf(".") < places+1) {
+            s += "0";
+        }
+
+        return s;
+    }
+
+
+    /**
+     * Method to wait on string from keyboard.
+     * @param s prompt string to print
+     * @return string typed in keyboard
+     */
+    public String inputStr(String s) {
+        String aLine = "";
+        BufferedReader input =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(s);
+        try {
+            aLine = input.readLine();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return aLine;
+    }
+
+
+    class sendDataThread extends Thread {
+
+        public void run() {
+
+System.out.println("Send thread started");
+
+            // in the arg order:
+            int rocID = 123;
+            int eventID = 10000;
+            int dataBankTag = 666; // starting data bank tag
+            int dataBankNum = 777; // starting data bank num
+            int eventNumber = 1;
+            int numEventsInPayloadBank = 1; // number of physics events in payload bank
+            int timestamp = 1000;
+            int recordId = 1;
+            int numPayloadBanks = 1;
+
+            int rocNum;
+            EvioEvent ev;
+            ByteBuffer bbuf = ByteBuffer.allocate(1000);
+            cMsgMessage msg = new cMsgMessage();
+//            msg.setSubject(subject);
+            msg.setType(type);
+
+            long counter = 0;
+            StringWriter sw = new StringWriter(2048);
+            PrintWriter  wr = new PrintWriter(sw, true);
+            long start_time = System.currentTimeMillis();
+
+            try {
+
+                while (true) {
+                    // send transport records from 3 ROCs
+                    rocNum = rocID;
+
+                    for (int ix = 0; ix < 3; ix++) {
+                        // send event over network
+     System.out.println("Send roc record " + rocID + " over network");
+                        // turn event into byte array
+//                        ev = Evio.createDataTransportRecord(rocNum, eventID,
+//                                                            dataBankTag, dataBankNum,
+//                                                            eventNumber, numEventsInPayloadBank,
+//                                                            timestamp, recordId, numPayloadBanks);
+
+
+                        // traditional bank of banks
+                        EventBuilder eventBuilder = new EventBuilder(0x1234, DataType.BANK, 1);
+                        EvioEvent event = eventBuilder.getEvent();
+
+                        // add a bank of ints
+                        EvioBank bank = new EvioBank(0x22, DataType.INT32, 0); // tag, type, num
+                        int[] iarray = {1,2,3};
+                        eventBuilder.appendIntData(bank, iarray);
+                        eventBuilder.addChild(event, bank);
+
+                        // add a bank of segments
+                        EvioBank bank2 = new EvioBank(0x33, DataType.SEGMENT, 0);
+
+                        EvioSegment segment1 = new EvioSegment(0x34, DataType.SHORT16);
+                        short[] sarray = {4,5,6};
+                        eventBuilder.appendShortData(segment1, sarray);
+                        eventBuilder.addChild(bank2, segment1);
+                        eventBuilder.addChild(event, bank2);
+
+                        // add a bank of tag segments
+                        EvioBank bank3 = new EvioBank(0x45, DataType.TAGSEGMENT, 0);
+
+                        EvioTagSegment tagsegment1 = new EvioTagSegment(0x46, DataType.DOUBLE64);
+                        double[] darray = {7,8,9};
+                        eventBuilder.appendDoubleData(tagsegment1, darray);
+                        eventBuilder.addChild(bank3, tagsegment1);
+                        eventBuilder.addChild(event, bank3);
+
+
+                        sw.getBuffer().delete(0, sw.getBuffer().capacity());
+                        bbuf.clear();
+                        event.write(bbuf);
+                        bbuf.flip();
+                        
+                        System.out.println("Sending array of length " + bbuf.limit());
+                        msg.setByteArrayNoCopy(bbuf.array(),0,bbuf.limit());
+                        msg.setByteArrayEndian(cMsgConstants.endianBig);
+                        msg.setSubject(subject1);
+                        coda.send(msg);
+                        msg.setSubject(subject2);
+                        coda.send(msg);
+
+                        StringWriter sw2 = new StringWriter(1000);
+                        XMLStreamWriter xmlWriter = XMLOutputFactory.newInstance().createXMLStreamWriter(sw2);
+                        event.toXML(xmlWriter);
+                        System.out.println("\nSending msg:\n" + sw2.toString());
+//                        System.out.println("Sending msg (bin):");
+//                        while (bbuf.hasRemaining()) {
+//                            wr.printf("%#010x\n", bbuf.getInt());
+//                        }
+//                        System.out.println(sw.toString() + "\n\n");
+                    
+                        // send from next roc
+                        rocNum++;
+
+                        if (stopSending) {
+                            return;
+                        }
+
+                        Thread.sleep(2000);
+                    }
+
+                    if (counter++ % 10 == 0) {
+                        long now = System.currentTimeMillis();
+                        wr.printf("%d  Hz\n", 10L/(now - start_time));
+                        System.out.println(sw.toString());
+                        start_time = now;
+                    }
+
+                    eventID++;
+                    timestamp += numEventsInPayloadBank;
+                    eventNumber += numEventsInPayloadBank;
+                }
+
+            }
+            catch (XMLStreamException e) {
+                e.printStackTrace();
+            }
+            catch (EvioException e) {
+                e.printStackTrace();
+            }
+            catch (cMsgException e) {
+                e.printStackTrace();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return;
+        }
+
+    }
+
+
+    /**
+     * This method is executed as a thread.
+     */
+    public void run() throws cMsgException {
+
+        if (debug) {
+            System.out.println("Running SenderCmsg as EMU's ROC1\n");
+        }
+
+        // Get the UDL for connection to the cMsg server. If none given,
+        // cMSGPortal defaults to using UDL = "cMsg:cMsg://localhost/cMsg/test".
+        if (UDL == null) UDL = System.getProperty("cmsgUDL");
+        if (UDL == null) UDL = "cMsg://localhost/cMsg/test";
+
+        // connect to cMsg server
+        coda = new cMsg(UDL, name, description);
+        coda.connect();
+
+        // enable message reception
+        coda.start();
+
+        inputStr("Enter to GO");
+        stopSending = false;
+
+        sendDataThread sender = new sendDataThread();
+        sender.start();
+
+        inputStr("Enter to QUIT");
+        stopSending = true;
+
+        try { Thread.sleep(10000); }
+        catch (InterruptedException e) { }
+
+    }
+
+
+}
