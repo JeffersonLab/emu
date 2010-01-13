aab8b2c148c247a3610658056f91a3b5825b57b0
Carl Timmer <timmer@jlab.org>
2010-01-13 16:28:05 +0000

    make members final

--- java/org/jlab/coda/emu/support/messaging/RCConstants.java
+++ java/org/jlab/coda/emu/support/messaging/RCConstants.java
@@ -14,73 +14,72 @@ package org.jlab.coda.emu.support.messaging;
 /**
  * This class holds cMsg message types that are used for both subscriptions and messages.
  * @author timmer
- * @date 1/6/2010
+ * 1/6/2010
  */
 public class RCConstants {
 
     // Subscriptions
 
     /** String used as type in subscription that receives transition commands. */
-    static public String transitionCommandType = "run/transition/*";
-
+    public static final String transitionCommandType = "run/transition/*";
     /** String used as type in subscription that receives run commands. */
-    static public String runCommandType        = "run/control/*";
-
+    public static final String runCommandType        = "run/control/*";
     /** String used as type in subscription that receives session commands. */
-    static public String sessionCommandType    = "session/control/*";
+    public static final String sessionCommandType    = "session/control/*";
 
     // Types in messages sent
 
     /** String used as type of status message periodically sent to rcServer. */
-    static public String reportStatus     = "rc/report/status";
+    public static final String reportStatus     = "rc/report/status";
 
     // Types in messages received in session/control/* callback
 
     /** String used as type in message for the start-reporting session command. */
-    static public String startReporting   = "session/control/startReporting";
-
+    public static final String startReporting   = "session/control/startReporting";
     /** String used as type in message for the stop-reporting session command. */
-    static public String stopReporting    = "session/control/stopReporting";
-
+    public static final String stopReporting    = "session/control/stopReporting";
     /** String used as type in message for the get-run-type session command. */
-    static public String getRunType       = "session/control/getRunType";
-
+    public static final String getRunType       = "session/control/getRunType";
     /** String used as type in message for the set-run-type session command. */
-    static public String setRunType       = "session/control/setRunType";
-
+    public static final String setRunType       = "session/control/setRunType";
     /** String used as type in message for the get-run-number session command. */
-    static public String getRunNumber     = "session/control/getRunNumber";
-
+    public static final String getRunNumber     = "session/control/getRunNumber";
     /** String used as type in message for the set-run-number session command. */
-    static public String setRunNumber     = "session/control/setRunNumber";
+    public static final String setRunNumber     = "session/control/setRunNumber";
 
     // Types in messages received in run/control/* callback
 
     /** String used as type in message for the set state run command. */
-    static public String setState         = "run/control/setState";
-
+    public static final String setState         = "run/control/setState";
     /** String used as type in message for the set session run command. */
-    static public String setSession       = "run/control/setSession";
-
+    public static final String setSession       = "run/control/setSession";
     /** String used as type in message for the get session run command. */
-    static public String getSession       = "run/control/getSession";
-
+    public static final String getSession       = "run/control/getSession";
     /** String used as type in message for the release session run command. */
-    static public String releaseSession   = "run/control/releaseSession";
-
+    public static final String releaseSession   = "run/control/releaseSession";
     /** String used as type in message for the set interval run command. */
-    static public String setInterval      = "run/control/setInterval";
-
+    public static final String setInterval      = "run/control/setInterval";
     /** String used as type in message for the start run command. */
-    static public String start            = "run/control/start";
-
+    public static final String start            = "run/control/start";
     /** String used as type in message for the stop run command. */
-    static public String stop             = "run/control/stop";
-
+    public static final String stop             = "run/control/stop";
     /** String used as type in message for the reset run command. */
-    static public String reset            = "run/control/reset";
-
+    public static final String reset            = "run/control/reset";
     /** String used as type in message for the exit run command. */
-    static public String exit             = "run/control/exit";
+    public static final String exit             = "run/control/exit";
 
+    // Payload names in reportStatus (above) message
+    
+    public static final String    name                 = "codaName";
+    public static final String    codaClass            = "codaClass";
+    public static final String    state                = "state";
+    public static final String    eventNumber          = "eventCount";
+    public static final String    eventRate            = "eventRate";
+    public static final String    dataRate             = "dataRate";
+    public static final String    numberOfLongs        = "dataCount";
+    public static final String    liveTime             = "liveTime";
+    public static final String    filename             = "fileName";
+    public static final String    objectType           = "objectType";
+    public static final String    runStartTime         = "runStartTime";
+    public static final String    runEndTime           = "runEndTime";
   }
