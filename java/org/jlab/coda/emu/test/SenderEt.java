b133d0b6676c59464e40c35c15097df77c321719
Carl Timmer <timmer@jlab.org>
2010-07-14 14:32:20 +0000

    save

--- java/org/jlab/coda/emu/test/SenderEt.java
+++ java/org/jlab/coda/emu/test/SenderEt.java
@@ -13,6 +13,7 @@ import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by IntelliJ IDEA.
@@ -154,6 +155,290 @@ public class SenderEt {
     }
 
 
+    /**
+     * This method is executed as a thread.
+     */
+    public void runMT() {
+
+        if (debug) {
+            System.out.println("Running SenderEt as EMU's ROC1,2,3\n");
+        }
+
+        try {
+            // make a direct connection to ET system's tcp server
+            EtSystemOpenConfig config = new EtSystemOpenConfig(etName, "localhost", 12347);
+
+            // create ET system object with verbose debugging output
+            sys = new EtSystem(config, EtConstants.debugInfo);
+
+            sys.open();
+
+            // get GRAND_CENTRAL station object
+            gc = sys.stationNameToObject("GRAND_CENTRAL");
+
+            // attach to grandcentral
+            att = sys.attach(gc);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // latch to start things off at the same time
+        CountDownLatch latch = new CountDownLatch(1);
+
+
+        stopSending = false;
+
+        // ROC 1
+        sendDataThreadMT sender = new sendDataThreadMT(1, true, false, latch);
+        sender.start();
+        // ROC 2
+        sender = new sendDataThreadMT(2, true, false, latch);
+        sender.start();
+        // ROC 3
+        sender = new sendDataThreadMT(3, true, false, latch);
+        sender.start();
+
+        inputStr("Enter to GO");
+        latch.countDown();
+        
+        inputStr("Enter to QUIT");
+        stopSending = true;
+
+        try { Thread.sleep(10000); }
+        catch (InterruptedException e) { }
+
+    }
+
+
+    class sendDataThreadMT extends Thread {
+
+        int rocNum;
+        boolean doStats;
+        boolean doUserEvents;
+        CountDownLatch latch;
+
+        sendDataThreadMT(int rocNum, boolean doStats, boolean doUserEvents, CountDownLatch latch) {
+            this.latch = latch;
+            this.rocNum = rocNum;
+            this.doStats = doStats;
+            this.doUserEvents = doUserEvents;
+        }
+
+        public void run() {
+
+            // one ET connection per thread
+//            EtSystem mySys;
+//            EtAttachment myAtt;
+//            EtStation myGc;
+//
+//            try {
+//                // make a direct connection to ET system's tcp server
+//                EtSystemOpenConfig config = new EtSystemOpenConfig(etName, "localhost", 12347);
+//
+//                // create ET system object with verbose debugging output
+//                mySys = new EtSystem(config, EtConstants.debugInfo);
+//
+//                mySys.open();
+//
+//                // get GRAND_CENTRAL station object
+//                myGc = mySys.stationNameToObject("GRAND_CENTRAL");
+//
+//                // attach to grandcentral
+//                myAtt = mySys.attach(myGc);
+//            }
+//            catch (Exception e) {
+//                e.printStackTrace();
+//                return;
+//            }
+
+            // in the arg order:
+            int eventID = 10000;
+            int dataBankTag = 666; // starting data bank tag
+            int dataBankNum = 777; // starting data bank num
+            int eventNumber = 1;
+            int numEventsInPayloadBank = 1; // number of physics events in payload bank
+            int timestamp = 1000;
+            int recordId = 1;
+            int numPayloadBanks = 1;
+
+            EvioEvent ev;
+            ByteBuffer bbuf;
+            int[] control = new int[EtConstants.stationSelectInts];
+            Arrays.fill(control, -1);
+
+            int chunk = 100, ignore = 3;
+            long counter = 0L;
+            long start_time = System.currentTimeMillis();
+            long totalT=0L, totalC=0L;
+
+
+            EvioBank bank;
+            EventBuilder builder = new EventBuilder(0, DataType.BANK, 0); // this event not used, just need a builder
+
+            // create user event to be inserted to test EMU building algorithm
+            int dtrTag = Evio.createCodaTag(EventType.USER.getValue(), 1);  // Tag(user value, source id)
+            EvioEvent dtrEvent = new PayloadBank(dtrTag, DataType.BANK, 1);
+            builder.setEvent(dtrEvent);
+
+            try {
+                // add bank with full recordId
+                bank = new EvioBank(Evio.RECORD_ID_BANK, DataType.INT32, 1);
+                bank.appendIntData(new int[] {1});
+                builder.addChild(dtrEvent, bank);
+                bank = new EvioBank(123, DataType.INT32, 456);
+                bank.appendIntData(new int[] {123});
+                builder.addChild(dtrEvent, bank);
+                dtrEvent.setAllHeaderLengths();  // TODO: necessary?
+
+            } catch (EvioException e) {/* never happen */}
+
+
+            try {
+
+                int loops = 0;
+
+                latch.await();
+
+                while (true) {
+                    EtEvent[] evs = sys.newEvents(att, Mode.SLEEP, 0, chunk, 1024, rocNum);
+                    if (evs.length < chunk) {
+                        sys.dumpEvents(att, evs);
+                        continue;
+                    }
+
+                    // stations will filter events on this number
+                    control[0] = rocNum;
+
+                    // put user event(s) into the event flow from this "ROC"
+                    if (doUserEvents) {
+                        if (loops++ % 1000 == 0) {
+                            EtEvent[] userEvs = sys.newEvents(att, Mode.SLEEP, 0, 1, 1024);
+                            for (EtEvent userEv : userEvs) {
+                                bbuf = userEv.getDataBuffer();
+                                userEv.setLength(bbuf.limit());
+                                userEv.setControl(control);
+                                dtrEvent.write(bbuf);
+                            }
+                            System.out.println("Putting " + userEvs.length + " user events in");
+                            sys.putEvents(att, userEvs);
+                        }
+                    }
+
+
+                    for (int j = 0; j < chunk; j++) {
+                        // turn event into byte array
+                        ev = Evio.createDataTransportRecord(rocNum, eventID,
+                                                            dataBankTag, dataBankNum,
+                                                            eventNumber, numEventsInPayloadBank,
+                                                            timestamp, recordId, numPayloadBanks,
+                                                            false);
+                        bbuf = evs[j].getDataBuffer();
+                        evs[j].setLength(bbuf.limit());
+                        evs[j].setControl(control);
+                        ev.write(bbuf);
+
+                        if (stopSending) {
+                            return;
+                        }
+
+                        recordId++;
+                        eventID++;
+                        timestamp   += numEventsInPayloadBank;
+                        eventNumber += numEventsInPayloadBank;
+                    }
+
+                    sys.putEvents(att, evs);
+
+                    counter += evs.length;
+
+                    Thread.sleep(delay);
+
+                    // only need stats from one of these threads
+                    if (doStats) {
+                        long now = System.currentTimeMillis();
+                        long deltaT = now - start_time;
+                        if (deltaT > 2000) {
+                            String s;
+                            if (ignore < 1) {
+                                totalC += counter;
+                                totalT += deltaT;
+                                s = String.format("%d:  %d Hz, avg %d Hz", rocNum, counter*1000/deltaT, totalC*1000/totalT);
+                            }
+                            // don't count the first few rates (slow) as part of the average
+                            else {
+                                s = String.format("%d Hz", counter*1000/deltaT);
+                                ignore--;
+                            }
+                            System.out.println(s);
+                            start_time = now;
+                            counter = 0;
+                        }
+                    }
+                }
+
+            }
+            catch (EvioException e) {
+                e.printStackTrace();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return;
+        }
+
+    }
+
+
+    /**
+      * This method is executed as a thread.
+      */
+     public void run() {
+
+         if (debug) {
+             System.out.println("Running SenderEt as EMU's ROC1,2,3\n");
+         }
+
+         try {
+             // make a direct connection to ET system's tcp server
+             EtSystemOpenConfig config = new EtSystemOpenConfig(etName, "localhost", 12347);
+
+             // create ET system object with verbose debugging output
+             sys = new EtSystem(config, EtConstants.debugInfo);
+
+             sys.open();
+
+             // get GRAND_CENTRAL station object
+             gc = sys.stationNameToObject("GRAND_CENTRAL");
+
+             // attach to grandcentral
+             att = sys.attach(gc);
+         }
+         catch (Exception e) {
+             e.printStackTrace();
+             return;
+         }
+
+
+         inputStr("Enter to GO");
+         stopSending = false;
+
+         sendDataThread sender = new sendDataThread();
+         sender.start();
+
+         inputStr("Enter to QUIT");
+         stopSending = true;
+
+         try { Thread.sleep(10000); }
+         catch (InterruptedException e) { }
+
+     }
+
     class sendDataThread extends Thread {
 
         public void run() {
@@ -177,9 +462,10 @@ System.out.println("Send thread started");
             int[] control = new int[EtConstants.stationSelectInts];
             Arrays.fill(control, -1);
 
-            int index, chunk = 300;
-            int counter = 0;
+            int index, chunk = 300, ignore = 3;
+            long counter = 0L;
             long start_time = System.currentTimeMillis();
+            long totalT=0L, totalC=0L;
 
 
             EvioBank bank;
@@ -213,16 +499,18 @@ System.out.println("Send thread started");
                         continue;
                     }
 
-                    if (loops++ % 1000 == 0) {
-                        EtEvent[] userEvs = sys.newEvents(att, Mode.SLEEP, 0, 1, 1024);
-                        bbuf = userEvs[0].getDataBuffer();
-                        userEvs[0].setLength(bbuf.limit());
-                        control[0] = 1;
-                        userEvs[0].setControl(control);
-                        dtrEvent.write(bbuf);
-System.out.println("Putting user event in");
-                        sys.putEvents(att, userEvs);
-                    }
+//                    if (loops++ % 1000 == 0) {
+//                        EtEvent[] userEvs = sys.newEvents(att, Mode.SLEEP, 0, 5, 1024);
+//                        for (EtEvent userEv : userEvs) {
+//                            bbuf = userEv.getDataBuffer();
+//                            userEv.setLength(bbuf.limit());
+//                            control[0] = 1;
+//                            userEv.setControl(control);
+//                            dtrEvent.write(bbuf);
+//                        }
+//                        System.out.println("Putting " + userEvs.length + " user events in");
+//                        sys.putEvents(att, userEvs);
+//                    }
 
                     for (int j = 0; j < chunk; j += 3) {
                         // send transport records from 3 ROCs
@@ -264,7 +552,17 @@ System.out.println("Putting user event in");
                     long now = System.currentTimeMillis();
                     long deltaT = now - start_time;
                     if (deltaT > 2000) {
-                        String s = String.format("%d  Hz", counter*1000/deltaT);
+                        String s;
+                        if (ignore < 1) {
+                            totalC += counter;
+                            totalT += deltaT;
+                            s = String.format("%d Hz, avg %d Hz", counter*1000/deltaT, totalC*1000/totalT);
+                        }
+                        // don't count the first few rates (slow) as part of the average
+                        else {
+                            s = String.format("%d Hz", counter*1000/deltaT);
+                            ignore--;
+                        }
                         System.out.println(s);
                         start_time = now;
                         counter = 0;
@@ -272,9 +570,6 @@ System.out.println("Putting user event in");
                 }
 
             }
-//            catch (XMLStreamException e) {
-//                e.printStackTrace();
-//            }
             catch (EvioException e) {
                 e.printStackTrace();
             }
@@ -287,50 +582,6 @@ System.out.println("Putting user event in");
 
             return;
         }
-
-    }
-
-
-    /**
-     * This method is executed as a thread.
-     */
-    public void run() {
-
-        if (debug) {
-            System.out.println("Running SenderEt as EMU's ROC1,2,3\n");
-        }
-
-        try {
-            // make a direct connection to ET system's tcp server
-            EtSystemOpenConfig config = new EtSystemOpenConfig(etName, "localhost", 12347);
-
-            // create ET system object with verbose debugging output
-            sys = new EtSystem(config, EtConstants.debugInfo);
-
-            sys.open();
-
-            // get GRAND_CENTRAL station object
-            gc = sys.stationNameToObject("GRAND_CENTRAL");
-
-            // attach to grandcentral
-            att = sys.attach(gc);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-
-        inputStr("Enter to GO");
-        stopSending = false;
-
-        sendDataThread sender = new sendDataThread();
-        sender.start();
-
-        inputStr("Enter to QUIT");
-        stopSending = true;
-
-        try { Thread.sleep(10000); }
-        catch (InterruptedException e) { }
 
     }
 
