5da20a6d184c6f2bfb995e094aab981e0bf8b7a3
Carl Timmer <timmer@jlab.org>
2011-04-28 18:01:40 +0000

    change layout manager

--- java/org/jlab/coda/emu/support/ui/HelpBox.java
+++ java/org/jlab/coda/emu/support/ui/HelpBox.java
@@ -15,9 +15,6 @@
 
 package org.jlab.coda.emu.support.ui;
 
-import com.intellij.uiDesigner.core.GridConstraints;
-import com.intellij.uiDesigner.core.GridLayoutManager;
-
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
@@ -63,7 +60,7 @@ public class HelpBox extends JDialog {
 
             //======== contentPanel ========
             {
-                contentPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+                contentPanel.setLayout(new GridLayout());
 
                 //======== scrollPane1 ========
                 {
@@ -72,11 +69,7 @@ public class HelpBox extends JDialog {
                     textPane1.setText(bundle.getString("HelpWindow"));
                     scrollPane1.setViewportView(textPane1);
                 }
-                contentPanel.add(scrollPane1, new GridConstraints(0, 0, 1, 1,
-                    GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH,
-                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-                    GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
-                    null, null, null));
+                contentPanel.add(scrollPane1);
             }
             dialogPane.add(contentPanel, BorderLayout.CENTER);
 
