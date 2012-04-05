edbd70b2ac4baf2d35c94f5049a27ae27d369a26
Carl Timmer <timmer@jlab.org>
2012-04-05 18:42:35 +0000

    comments

--- java/org/jlab/coda/emu/support/data/CODATag.java
+++ java/org/jlab/coda/emu/support/data/CODATag.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2012, Jefferson Science Associates
+ *
+ * Thomas Jefferson National Accelerator Facility
+ * Data Acquisition Group
+ *
+ * 12000, Jefferson Ave, Newport News, VA 23606
+ * Phone : (757)-269-7100
+ *
+ */
+
 package org.jlab.coda.emu.support.data;
 
 /**
