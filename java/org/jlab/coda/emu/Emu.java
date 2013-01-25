c160f343447fc98c8cf8965587a5509b8d2b6886
Carl Timmer <timmer@jlab.org>
2013-01-25 16:30:06 +0000

    add printout to see name and type

--- java/org/jlab/coda/emu/Emu.java
+++ java/org/jlab/coda/emu/Emu.java
@@ -497,6 +497,7 @@ public class Emu implements CODAComponent {
                 codaClass = cc;
             }
         }
+System.out.println("Emu created, name = " + name + ", type = " + codaClass);
 
         this.name = name;
         this.cmsgUDL = cmsgUDL;  // may be null
