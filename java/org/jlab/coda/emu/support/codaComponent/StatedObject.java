30d09374a4ab67c719e1199d9deffeb81b13e835
Carl Timmer <timmer@jlab.org>
2013-05-01 15:40:57 +0000

    remove name() method from interface

--- java/org/jlab/coda/emu/support/codaComponent/StatedObject.java
+++ java/org/jlab/coda/emu/support/codaComponent/StatedObject.java
@@ -1,20 +1,25 @@
+/*
+ * Copyright (c) 2008, Jefferson Science Associates
+ *
+ * Thomas Jefferson National Accelerator Facility
+ * Data Acquisition Group
+ *
+ * 12000, Jefferson Ave, Newport News, VA 23606
+ * Phone : (757)-269-7100
+ *
+ */
+
 package org.jlab.coda.emu.support.codaComponent;
 
 import org.jlab.coda.emu.support.control.State;
 
 /**
- * An object that implements this interface has a name and a state.
+ * An object that implements this interface has a state.
  */
 public interface StatedObject {
     /**
-     * Get the name of this object.
-     * @return the name of this object
-     */
-    public String name();
-
-    /**
      * Get the state of this object.
-     * @return the state of this Cobject
+     * @return the state of this object
      */
     public State state();
 }
