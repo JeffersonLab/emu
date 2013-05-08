ac729e471e92fcc8fd734156255a9deb003ae634
Carl Timmer <timmer@jlab.org>
2013-05-08 15:02:41 +0000

    add getError method

--- java/org/jlab/coda/emu/support/codaComponent/StatedObject.java
+++ java/org/jlab/coda/emu/support/codaComponent/StatedObject.java
@@ -13,11 +13,21 @@ package org.jlab.coda.emu.support.codaComponent;
 
 /**
  * An object that implements this interface has a state.
+ * @author heyes
+ * @author timmer
  */
 public interface StatedObject {
+
     /**
      * Get the state of this object.
      * @return the state of this object
      */
     public State state();
+
+    /**
+     * Get any available error information.
+     * @return any available error information; null if none
+     */
+    public String getError();
+
 }
