e8241a419dc12a428fa93543ed011df08a97d11b
Carl Timmer <timmer@jlab.org>
2013-05-09 19:10:05 +0000

    javadoc

--- java/org/jlab/coda/emu/support/transport/DataTransportImplFifo.java
+++ java/org/jlab/coda/emu/support/transport/DataTransportImplFifo.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2008, Jefferson Science Associates
+ *
+ * Thomas Jefferson National Accelerator Facility
+ * Data Acquisition Group
+ *
+ * 12000, Jefferson Ave, Newport News, VA 23606
+ * Phone : (757)-269-7100
+ *
+ */
+
 package org.jlab.coda.emu.support.transport;
 
 import org.jlab.coda.emu.Emu;
@@ -7,25 +18,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Implement a DataTransport that uses DataChannel based on BlockingQueue.
- * 1 Fifo transport object for each EMU object.
+ * Implement a DataTransport that creates FIFO DataChannels based on the BlockingQueue.
+ * There is one Fifo transport object for each EMU.
  *
  * @author heyes
  * @author timmer
  * Date: Nov 10, 2008
  */
 public class DataTransportImplFifo extends DataTransportAdapter {
+
     /** Map of all Fifo channels. */
     private final HashMap<String, DataChannel> allChannels = new HashMap<String, DataChannel>();
 
+    /**
+     * Constructor.
+     *
+     * @param pname  name of transport
+     * @param attrib transport's attribute map from config file
+     * @param emu  emu object this transport belongs to
+     *
+     * @throws DataNotFoundException when "server" and "class" attributes
+     *                               are missing from attrib map.
+     */
     public DataTransportImplFifo(String pname, Map<String, String> attrib, Emu emu) throws DataNotFoundException {
         super(pname, attrib, emu);
     }
 
+    /** {@inheritDoc}. Remove all fifo channels. */
     public void end() {
         allChannels.clear();
     }
 
+    /** {@inheritDoc}. Remove all fifo channels. */
     public void reset() {
          allChannels.clear();
     }
