043689b19f7197fc555f67b5e6254b503e82e70b
Carl Timmer <timmer@jlab.org>
2013-05-10 15:16:38 +0000

    javadoc

--- java/org/jlab/coda/emu/support/data/QueueItemType.java
+++ java/org/jlab/coda/emu/support/data/QueueItemType.java
@@ -1,8 +1,20 @@
+/*
+ * Copyright (c) 2013, Jefferson Science Associates
+ *
+ * Thomas Jefferson National Accelerator Facility
+ * Data Acquisition Group
+ *
+ * 12000, Jefferson Ave, Newport News, VA 23606
+ * Phone : (757)-269-7100
+ *
+ */
+
 package org.jlab.coda.emu.support.data;
 
 /**
+ * This class enumerates the possible types of data that can be used in an EMU.
  * @author timmer
- * @Date 4/3/13
+ * (4/3/13)
  */
 public enum QueueItemType {
 
