c11daab508520fd1a2ea8c3b59dccebcd3e44630
Carl Timmer <timmer@jlab.org>
2013-05-10 15:51:28 +0000

    javadoc

--- java/org/jlab/coda/emu/EmuException.java
+++ java/org/jlab/coda/emu/EmuException.java
@@ -61,8 +61,7 @@ public class EmuException extends Exception {
      * message of <tt>(cause==null ? null : cause.toString())</tt> (which
      * typically contains the class and detail message of <tt>cause</tt>).
      * This constructor is useful for exceptions that are little more than
-     * wrappers for other throwables (for example, {@link
-     * java.security.PrivilegedActionException}).
+     * wrappers for other throwables.
      *
      * @param cause the cause (which is saved for later retrieval by the
      *              {@link #getCause()} method).  (A <tt>null</tt> value is
