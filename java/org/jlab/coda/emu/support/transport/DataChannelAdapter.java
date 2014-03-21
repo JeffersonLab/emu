d6bcb5d2d7be82eb238367073afcd861519de575
Carl Timmer <timmer@jlab.org>
2014-03-21 15:19:39 +0000

    store recordId, module that channel belongs to  and allows for multiple queues per channel

--- java/org/jlab/coda/emu/support/transport/DataChannelAdapter.java
+++ java/org/jlab/coda/emu/support/transport/DataChannelAdapter.java
@@ -14,6 +14,7 @@ package org.jlab.coda.emu.support.transport;
 
 import org.jlab.coda.emu.Emu;
 import org.jlab.coda.emu.EmuEventNotify;
+import org.jlab.coda.emu.EmuModule;
 import org.jlab.coda.emu.support.codaComponent.CODAStateMachineAdapter;
 import org.jlab.coda.emu.support.codaComponent.State;
 import org.jlab.coda.emu.support.data.QueueItem;
@@ -21,19 +22,21 @@ import org.jlab.coda.emu.support.data.QueueItemType;
 import org.jlab.coda.emu.support.logger.Logger;
 
 import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * This class provides a bare-bones implementation of the DataChannel
+ * This class provides boilerplate code for the DataChannel
  * interface (which includes the CODAStateMachine interface).
  * Extending this class implements the DataChannel interface and frees
  * any subclass from having to implement common methods or those that aren't used.<p>
  * This class defines an object that can send and
  * receive banks of data in the CODA evio format. It
- * refers to a particular connection (eg. a single socket
+ * refers to a particular connection (eg. an et open
  * or cMsg connection id).
  *
  * @author timmer
@@ -43,6 +46,9 @@ public class DataChannelAdapter extends CODAStateMachineAdapter implements DataC
 
     /** Channel id (corresponds to sourceId of ROCs for CODA event building). */
     protected int id;
+
+    /** Record id (corresponds to evio events flowing through data channel). */
+    protected int recordId;
 
     /** Channel state. */
     protected State state;
@@ -72,14 +78,26 @@ public class DataChannelAdapter extends CODAStateMachineAdapter implements DataC
     /** Object used by Emu to be notified of END event arrival. */
     protected EmuEventNotify endCallback;
 
+    /** Module to which this channel belongs. */
+    protected final EmuModule module;
+
     /** Object used by Emu to create this channel. */
     protected final DataTransport dataTransport;
 
     /** Type of object to expect in each queue item. */
     protected QueueItemType queueItemType;
 
-    /** Queue used to hold data for either input or output depending on {@link #input}. */
+    /** First queue used to hold data for either input or output depending on {@link #input}. */
     protected final BlockingQueue<QueueItem> queue;
+
+    /** Number of data queues (does not include controlQ). */
+    protected int qCount;
+
+    /** List holding all queues used to hold data for either input or output depending on {@link #input}. */
+    protected final List<BlockingQueue<QueueItem>> queueList;
+
+    /** Queue used to hold incoming, non-buildable, control, & user events. */
+    protected final BlockingQueue<QueueItem> controlQ;
 
 
 
@@ -93,27 +111,55 @@ public class DataChannelAdapter extends CODAStateMachineAdapter implements DataC
      * @param attributeMap  the hashmap of config file attributes for this channel
      * @param input         true if this is an input data channel, otherwise false
      * @param emu           emu this channel belongs to
-     * @param queueItemType type of object to expect in queue item
+     * @param module        module this channel belongs to
     */
     public DataChannelAdapter(String name, DataTransport transport,
                               Map<String, String> attributeMap,
                               boolean input, Emu emu,
-                              QueueItemType queueItemType) {
+                              EmuModule module) {
         this.emu = emu;
         this.name = name;
         this.input = input;
-        this.queueItemType = queueItemType;
+        this.module = module;
         this.dataTransport = transport;
         logger = emu.getLogger();
 
-        // Set queue capacity.
+        if (input) {
+            queueItemType = module.getInputQueueItemType();
+        }
+        else {
+            queueItemType = module.getOutputQueueItemType();
+        }
+
+        // Set queue capacity
         // 100 buffers * 100 events/buf * 220 bytes/Roc/ev =  2.2Mb/Roc
         int capacity = 100;
         try {
             capacity = dataTransport.getIntAttr("capacity");
         }
         catch (Exception e) {}
-        queue = new LinkedBlockingQueue<QueueItem>(capacity);
+
+
+        // Set number of data queues
+        qCount = 1;
+        try {
+            qCount = module.getIntAttr("qCount");
+            if (qCount < 1) {
+                qCount = 1;
+            }
+        }
+        catch (Exception e) {}
+
+
+        // Create data queues
+        queueList = new ArrayList<BlockingQueue<QueueItem>>(qCount);
+        for (int i=0; i < qCount; i++) {
+            queueList.add(new LinkedBlockingQueue<QueueItem>(capacity));
+        }
+        queue = queueList.get(0);
+
+        // Create non-data queue
+        controlQ = new LinkedBlockingQueue<QueueItem>(20);
 
 
         // Set id number. Use any defined in config file, else use default = 0
@@ -139,6 +185,15 @@ public class DataChannelAdapter extends CODAStateMachineAdapter implements DataC
 
     /** {@inheritDoc} */
     public int getID() {return id;}
+
+    /** {@inheritDoc} */
+    public void setID(int id) {this.id = id;}
+
+    /** {@inheritDoc} */
+    public int getRecordId() {return recordId;}
+
+    /** {@inheritDoc} */
+    public void setRecordId(int recordId) {this.recordId = recordId;}
 
     /** {@inheritDoc} */
     public State state() {return state;}
@@ -169,6 +224,15 @@ public class DataChannelAdapter extends CODAStateMachineAdapter implements DataC
 
     /** {@inheritDoc} */
     public BlockingQueue<QueueItem> getQueue() {return queue;}
+
+    /** {@inheritDoc} */
+    public int getQCount() {return qCount;}
+
+    /** {@inheritDoc} */
+    public List<BlockingQueue<QueueItem>> getQueueList() {return queueList;}
+
+    /** {@inheritDoc} */
+    public BlockingQueue<QueueItem> getControlQ() {return controlQ;}
 
     /** {@inheritDoc} */
     public void registerEndCallback(EmuEventNotify callback) {endCallback = callback;}
