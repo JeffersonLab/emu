ecb806a19070ccf09af0bcdd1ebe706da099c965
Carl Timmer <timmer@jlab.org>
2014-08-13 14:52:19 +0000

    javadoc

--- java/org/jlab/coda/emu/support/data/RingItemFactory.java
+++ java/org/jlab/coda/emu/support/data/RingItemFactory.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2014, Jefferson Science Associates
+ *
+ * Thomas Jefferson National Accelerator Facility
+ * Data Acquisition Group
+ *
+ * 12000, Jefferson Ave, Newport News, VA 23606
+ * Phone : (757)-269-7100
+ *
+ */
+
 package org.jlab.coda.emu.support.data;
 
 import com.lmax.disruptor.EventFactory;
@@ -5,21 +16,22 @@ import org.jlab.coda.jevio.EvioEvent;
 
 
 /**
- * Class used by the Disruptor's RingBuffer to populate itself with PayloadBuffers.
+ * Class used by the Disruptor's RingBuffer to populate itself
+ * with PayloadBuffers or PayloadBanks.
  * @author timmer (4/7/14)
  */
 public class RingItemFactory implements EventFactory<RingItem> {
 
     /** There are multiple classes that extend RingItem, which do we construct? */
-    QueueItemType itemType;
+    ModuleIoType itemType;
 
-    public RingItemFactory(QueueItemType itemType) {
+    public RingItemFactory(ModuleIoType itemType) {
         this.itemType = itemType;
     }
 
 
     public RingItem newInstance() {
-        if (itemType == QueueItemType.PayloadBuffer) {
+        if (itemType == ModuleIoType.PayloadBuffer) {
             return new PayloadBuffer();
         }
         return new PayloadBank((EvioEvent)null);
