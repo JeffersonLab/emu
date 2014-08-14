5e9471c95b52e641f2d140f0a814ad4a8192851a
Carl Timmer <timmer@jlab.org>
2014-08-14 19:46:00 +0000

    get platform's host IPs and cmsg domain TCP server port in order to connect to and use that server

--- java/org/jlab/coda/emu/support/messaging/CMSGPortal.java
+++ java/org/jlab/coda/emu/support/messaging/CMSGPortal.java
@@ -13,15 +13,12 @@ package org.jlab.coda.emu.support.messaging;
 
 import org.jlab.coda.cMsg.*;
 import org.jlab.coda.emu.Emu;
+import org.jlab.coda.emu.EmuException;
 import org.jlab.coda.emu.support.codaComponent.CODAClass;
-import org.jlab.coda.emu.support.codaComponent.CODAComponent;
 import org.jlab.coda.emu.support.logger.Logger;
 import org.jlab.coda.emu.support.logger.LoggerAppender;
 import org.jlab.coda.emu.support.logger.LoggingEvent;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * This class creates a connection to a cMsg server for the
@@ -30,25 +27,25 @@ import java.util.Date;
 public class CMSGPortal implements LoggerAppender {
 
     /** Store a reference to the EMU here (which is the only object that uses this CMSGPortal object). */
-    final CODAComponent emu;
+    final Emu emu;
 
-    /** Connection object to rc multicast server of runcontrol platform. */
+    /** Connection object to rc multicast server of run control platform. */
     private cMsg rcServer;
 
-    /** UDL for connection to rc multicast server of runcontrol platform. */
-    private final String rcUDL;
+    /** UDL for connection to rc multicast server of run control platform. */
+    private String rcUDL;
 
-    /** Connection object to cMsg name server of runcontrol platform. */
+    /** Connection object to cMsg name server of run control platform. */
     private cMsg server;
 
-    /** UDL for connection to cMsg name server of runcontrol platform to send
+    /** UDL for connection to cMsg name server of run control platform to send
      *  messages internal to this emu. */
     private String UDL;
 
-    /** Connection object to cMsg name server of runcontrol platform for ROC's namespace. */
+    /** Connection object to cMsg name server of run control platform for ROC's namespace. */
     private cMsg rocServer;
 
-    /** UDL for connection to cMsg name server of runcontrol platform to send messages to
+    /** UDL for connection to cMsg name server of run control platform to send messages to
      *  and receive messages on a ROC. */
     private String rocUDL;
 
@@ -62,31 +59,24 @@ public class CMSGPortal implements LoggerAppender {
     /**
      * Constructor.
      * @param emu reference to EMU object
-     * @param expid experiment id
      */
-    public CMSGPortal(Emu emu, String expid) {
+    public CMSGPortal(Emu emu) throws EmuException {
         this.emu = emu;
+
         // UDL for connection to cMsg server was originally specified
         // with -DcmsgUDL=xxx flag to interpreter when running EMU.
-        String udl = emu.getCmsgUDL();
+        rcUDL = emu.getCmsgUDL();
 
-        // construct default UDL if necessary
-        if (udl == null) {
-            if (expid == null) {
-                expid = System.getenv("EXPID");
+        // Construct default UDL if necessary
+        if (rcUDL == null) {
+            if (emu.getExpid() == null) {
+                throw new EmuException("EXPID not defined");
             }
 
-            if (expid != null)  {
-                udl = "cMsg:rc://multicast/" + expid;
-            }
-            else {
-                udl = "cMsg:cMsg://localhost/cMsg/test";
-            }
+            rcUDL = "cMsg:rc://multicast/" + emu.getExpid();
         }
 
-        rcUDL = udl;
-
-//System.out.println("\n CMSGPortal using UDL = " + UDL + "\n");
+//System.out.println("\n CMSGPortal using rc UDL = " + rcUDL + "\n");
         logger = emu.getLogger();
         logger.addAppender(this);
 
@@ -111,47 +101,90 @@ public class CMSGPortal implements LoggerAppender {
             // for future use
             rcServer.subscribe("*", RCConstants.setOptionType, handler, null);
 
-            //--------------------------------------------
-            // Create a connection to the cMsg name server
-            //--------------------------------------------
-
-            // Find the host the platform (actually agent) is running on
-            // so we can do an cMsg domain connection.
-            cMsgMessage msg = rcServer.monitor("3000");
-            if (msg == null) {
-                System.out.println("\n\n PROBLEM: null msg in monitor\n\n");
-                platformHost = "localhost";
-            }
-            else {
-                platformHost = msg.getSenderHost();
-            }
-
-            // Only need one callback
-            MvalReportingHandler mHandler = new MvalReportingHandler(CMSGPortal.this);
-
-            // Install callback for reporting the smallest number of evio events per ET event
-
-            if (emu.getCodaClass() == CODAClass.SEB ||
-                emu.getCodaClass() == CODAClass.PEB)  {
-
-                // Use this connection for internal communications on this emu to set M value.
-                UDL = "cMsg://" + platformHost + "/cMsg/M";
-                server = new cMsg(UDL, emu.name()+"_emu", "EmuInternal");
-                server.connect();
-                server.start();
-                server.subscribe(this.emu.name(), "*", mHandler, null);
-
-                // cMsg subdomain with namespace expid on platform of cMsg server at default port
-                // Use this connection to send messages to the connected ROCs (through platform/agent)
-                rocUDL = "cMsg://" + platformHost + "/cMsg/" + expid;
-                rocServer = new cMsg(rocUDL, emu.name()+"_toRoc", "EmuToRoc");
-                rocServer.connect();
-            }
-
         } catch (Exception e) {
             e.printStackTrace();
             logger.warn("Exit due to rc/cMsg connect error: " + e.getMessage());
             System.exit(-1);
+        }
+    }
+
+
+    /**
+     * Create 2 connections to the run control platform's cMsg domain server.
+     * Called during the configure transition since that is when the rc
+     * server sends us info about the platform host and cMsg domain server's
+     * TCP port.
+     * @throws EmuException if platform did not send its IP addresses and cMsg TCP port;
+     *                      if cannot connect to cMsg domain server in platform.
+     */
+    synchronized public void cMsgServerConnect() throws  EmuException {
+
+ //            // Find the host the platform (actually agent) is running on
+ //            // so we can do an cMsg domain connection.
+ //            cMsgMessage msg = rcServer.monitor("3000");
+ //            if (msg == null) {
+ //                System.out.println("\n\n PROBLEM: null msg in monitor\n\n");
+ //                platformHost = "localhost";
+ //            }
+ //            else {
+ //                platformHost = msg.getSenderHost();
+ //            }
+
+        // Only need one callback
+        MvalReportingHandler mHandler = new MvalReportingHandler(CMSGPortal.this);
+
+        // Install callback for reporting the smallest number of evio events per ET event
+        if (emu.getCodaClass() == CODAClass.SEB ||
+            emu.getCodaClass() == CODAClass.PEB)  {
+
+            String goodAddress=null;
+            String[] addrs = emu.getPlatformIpAddresses();
+            if (addrs == null) {
+                throw new EmuException("Did not receive platform's IP addresses");
+            }
+            int tcpServerPort = emu.getPlatformTcpPort();
+            boolean foundServer = false;
+
+            // Use this connection for internal communications on this emu to set M value.
+            // But only need to do this once - at the first configure.
+            if (server == null) {
+                // To make a connection, try the IP addresses one-by-one
+                for (String ip : addrs) {
+                    UDL = "cMsg://" + ip + ":" + tcpServerPort + "/cMsg/M";
+                    try {
+                        server = new cMsg(UDL, emu.name()+"_emu", "EmuInternal");
+                        server.connect();
+                    }
+                    catch (cMsgException e) {
+                        continue;
+                    }
+                    foundServer = true;
+                    goodAddress = ip;
+                    break;
+                }
+
+                if (!foundServer) {
+                    throw new EmuException("Cannot connect to platform's cMsg domain server");
+                }
+
+                try {
+                    server.start();
+                    server.subscribe(this.emu.name(), "*", mHandler, null);
+
+                    // cMsg subdomain with namespace = expid on platform of cMsg server at default port
+                    // Use this connection to send messages to the connected ROCs (through platform/agent)
+                    // That happens while data is flowing.
+                    rocUDL = "cMsg://" + goodAddress + ":" + tcpServerPort + "/cMsg/" + emu.getExpid();
+                    rocServer = new cMsg(rocUDL, emu.name()+"_toRoc", "EmuToRoc");
+                    rocServer.connect();
+                }
+                catch (cMsgException e) {
+                    try {server.disconnect();}
+                    catch (cMsgException e1) {}
+                    server = null;
+                    throw new EmuException("Cannot connect to platform's cMsg domain server", e);
+                }
+            }
         }
     }
 
@@ -189,9 +222,14 @@ public class CMSGPortal implements LoggerAppender {
      * Get the connection object to the rc multicast server of runcontrol platform.
      * @return connection object to the rc multicast server of runcontrol platform.
      */
-    public cMsg getRcServer() {
-        return rcServer;
-    }
+    public cMsg getRcServer() {return rcServer;}
+
+
+    /**
+     * Get the UDL used to make the connection to the rc server.
+     * @return UDL used to make the connection to the rc server.
+     */
+    public String getRcUDL() {return rcUDL;}
 
 
     /**
