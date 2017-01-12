426a860b25efe30482cb759b51118a72e5119b16
Carl Timmer <timmer@jlab.org>
2017-01-12 21:57:19 +0000

    clean up

--- java/org/jlab/coda/emu/support/transport/DataChannelImplFile.java
+++ java/org/jlab/coda/emu/support/transport/DataChannelImplFile.java
@@ -23,7 +23,6 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
