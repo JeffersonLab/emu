e8ced031a64376a5ea40159584f2961bf6ce8be4
Carl Timmer <timmer@jlab.org>
2018-10-22 14:17:52 -0400

    turn garbage free operation back on

--- java/org/jlab/coda/emu/support/transport/DataChannelImplEmu.java
+++ java/org/jlab/coda/emu/support/transport/DataChannelImplEmu.java
@@ -211,7 +211,7 @@ public class DataChannelImplEmu extends DataChannelAdapter {
         }
 
         // set "garbage free" option on
-        useGarbageFree = false;
+        useGarbageFree = true;
         attribString = attributeMap.get("garbageFree");
         if (attribString != null) {
             if (attribString.equalsIgnoreCase("true") ||
