235deb1b15b10ddfff6667631069ffacb7e9cfdb
Carl Timmer <timmer@jlab.org>
2018-10-31 16:00:16 -0400

    remove printout

--- java/org/jlab/coda/emu/Emu.java
+++ java/org/jlab/coda/emu/Emu.java
@@ -1112,7 +1112,7 @@ System.out.println("\n\n");
                         reportMsg.addPayloadItem(new cMsgPayloadItem(RCConstants.destinationNames, outputDestinations));
                     }
                     else {
-                        System.out.println("\n\n******************* OUTPUT DESTINATIONS ********************\n");
+//System.out.println("\n\n******************* OUTPUT DESTINATIONS ********************\n");
                         reportMsg.removePayloadItem(RCConstants.filename);
                         reportMsg.removePayloadItem(RCConstants.destinationNames);
                     }
