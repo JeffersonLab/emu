4a732bf5341784aac48f63ca0fe1aa6454c2347a
Unknown <unknown@example.com>
2019-12-11 12:30:47 -0500

    clean up code

--- java/org/jlab/coda/emu/support/transport/DataChannelImplFile.java
+++ java/org/jlab/coda/emu/support/transport/DataChannelImplFile.java
@@ -844,7 +844,12 @@ logger.info("      DataChannel File out " + outputIndex + ": wrote GO");
                         // If this a user and "first event", let the writer know
                         if (ringItem.isFirstEvent()) {
                             if (emu.isFileWritingOn()) {
-                                evioFileWriter.setFirstEvent(ringItem.getNode());
+                                if (ringItem.getBuffer() != null) {
+                                    evioFileWriter.setFirstEvent(ringItem.getBuffer());
+                                }
+                                else {
+                                    evioFileWriter.setFirstEvent(ringItem.getNode());
+                                }
                             }
                             // The writer will handle the first event from here,
                             // go to the next event now.
