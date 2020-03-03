6808ebe55e57eee0a47cc94be114e5ee89cda63e
Unknown <unknown@example.com>
2020-03-03 10:21:30 -0500

    fix bug introduced with last check in

--- java/org/jlab/coda/emu/Emu.java
+++ java/org/jlab/coda/emu/Emu.java
@@ -737,7 +737,7 @@ System.out.println("Emu created, name = " + name + ", type = " + codaClass);
      */
     EmuModule getStatisticsModule() {
         synchronized(modules) {
-//            if (modules.size() < 1) return null;
+            if (modules.size() < 1) return null;
 //
 //            // Return first module that says its statistics represents EMU statistics
 //            for (EmuModule module : modules) {
@@ -750,7 +750,7 @@ System.out.println("Emu created, name = " + name + ", type = " + codaClass);
             // For now (2/2020), pick the last module, which will be the ER
 
             // If no modules claim to speak for EMU, choose last module in config file
-            return modules.get(modules.size()-1);
+            return modules.get(modules.size() - 1);
         }
     }
 
